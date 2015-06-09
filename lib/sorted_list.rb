3fe5cb7c16a869f4d217c156821730f5bf0e646e
Arsalan Ahmed <itsarsalan@yahoo.com>
2015-06-09 15:56:18 -0400

    implemented sort_by, select, and reduce methods

--- lib/sorted_list.rb
+++ lib/sorted_list.rb
@@ -1,3 +1,4 @@
+
 module FakeEnumerable
 	class SortedList
 		attr_accessor :list
@@ -12,14 +13,40 @@ module FakeEnumerable
 		end
 
 		def map
-			return self unless block_given?
-
-			mapped = SortedList.new
+			mapped = FakeEnumerable::SortedList.new
 			@list.each do |member|
 				mapped << yield(member)
 			end
-
 			mapped.list
 		end
+
+		def sort_by
+			map {|l| [yield(l), l]}.map {|l| l[1]}
+		end
+
+		def select
+			selected = FakeEnumerable::SortedList.new
+			@list.each do |member|
+				if yield(member)
+					selected << member 
+				end
+			end
+			selected.list
+		end
+
+		def reduce(how = nil)
+			reduced = how.is_a?(Fixnum) ? how : 0
+			symbol = how if how.is_a?(Symbol)
+
+			@list.each do |member|
+				if block_given?
+					reduced = yield(reduced, member)
+				else
+					reduced = reduced.send(symbol, member)
+				end
+			end
+			reduced
+		end
+
 	end
 end
--- spec/enumerable_tests.rb
+++ spec/enumerable_tests.rb
@@ -1,6 +1,7 @@
 require 'sorted_list'
 
 describe "FakeEnumerable::SortedList" do
+
   before do
     @list = FakeEnumerable::SortedList.new
 
@@ -8,12 +9,23 @@ describe "FakeEnumerable::SortedList" do
     @list << 3 << 13 << 42 << 4 << 7
   end
 
-  it "map without block returns original" do
-    mapped = @list.map
-    expect(mapped).to eql @list
-  end
-
   it "supports map" do
     expect(@list.map { |x| x + 1 }).to eql [4,5,8,14,43]
   end
+
+  it "supports sort_by" do
+    # ascii sort order
+    expect(@list.sort_by { |x| x.to_s }).to eql([13, 3, 4, 42, 7])
+  end
+
+  it "supports select" do
+    expect(@list.select { |x| x.even? }).to eql([4,42])
+  end
+
+  it "supports reduce" do
+    expect(@list.reduce(:+)).to eql(69)
+    expect(@list.reduce { |s,e| s + e }).to eql(69)
+    expect(@list.reduce(-10) { |s,e| s + e }).to eql(59)
+  end
+
 end
